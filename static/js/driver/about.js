c2749e7b2020095242405415bf0ed974e3d802b3
yaoleib <yaoleib@yonyou.com>
2018-08-15 10:28:37 +0800

    删除无用文件

--- static/js/driver/about.js
+++
@@ -1,7 +0,0 @@
-summerready = function(){
-    if ($summer.os == "android") {
-        $('#versionItem').css('display', 'block');
-    } else {
-        $('#versionItem').css('display', 'none');
-    }
-};
